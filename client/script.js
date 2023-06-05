5328c73de447750714373c24a3639d74229b009a
Anusthan Singh <102304867+anusthan12@users.noreply.github.com>
2023-06-05 22:54:40 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://aurora2.onrender.com', {
+    const response = await fetch('https://aurora-y9gz.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
